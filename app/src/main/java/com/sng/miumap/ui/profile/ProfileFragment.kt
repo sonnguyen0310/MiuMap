7a351643976556d058d217329f1068ee24aec926
Sonnguyenh <Sonnguyenh>
2020-03-09 14:39:00 -0500

    Init project

---
+++ app/src/main/java/com/sng/miumap/ui/profile/ProfileFragment.kt
@@ -0,0 +1,31 @@
+package com.sng.miumap.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.support.v4.app.Fragment
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import com.sng.miumap.R
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var profileViewModel: ProfileViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        profileViewModel =
+                ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        profileViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
