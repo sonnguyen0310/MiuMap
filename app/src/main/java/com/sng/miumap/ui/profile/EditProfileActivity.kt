68b42324ea11b754c6a02690217c23033facf0ea
Kanharith SOK <kanharithsok@gmail.com>
2020-03-11 15:25:35 -0500

    Added scrollview to EditProfile screen

--- app/src/main/java/com/sng/miumap/ui/profile/EditProfileActivity.kt
+++ app/src/main/java/com/sng/miumap/ui/profile/EditProfileActivity.kt
@@ -2,6 +2,7 @@ package com.sng.miumap.ui.profile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.sng.miumap.R
 import com.sng.miumap.model.Profile
 import com.squareup.picasso.Picasso
@@ -18,8 +19,9 @@ class EditProfileActivity : AppCompatActivity() {
         setupUI(profile)
     }
 
-    private fun setupUI(profile: Profile) {
-        Picasso.get().load(profile.imageUrl).into(profile_image_view)
+    private fun setupUI(profile: Profile?) {
+        val profile = profile ?: return
+        Picasso.get().load(profile.imageUrl).into(profile_image_view as ImageView)
         first_name_edit_text.setText(profile.firstName)
         last_name_edit_text.setText(profile.lastName)
         email_edit_text.setText(profile.email)
