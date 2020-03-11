8110aa5d41dcf84d452686cf06a2ef4598318359
Kanharith SOK <kanharithsok@gmail.com>
2020-03-11 15:52:48 -0500

    Added save button to edit profile screen

--- app/src/main/java/com/sng/miumap/ui/profile/EditProfileActivity.kt
+++ app/src/main/java/com/sng/miumap/ui/profile/EditProfileActivity.kt
@@ -2,6 +2,10 @@ package com.sng.miumap.ui.profile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.Log.d
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import com.sng.miumap.R
 import com.sng.miumap.model.Profile
@@ -21,14 +25,29 @@ class EditProfileActivity : AppCompatActivity() {
 
     private fun setupUI(profile: Profile?) {
         val profile = profile ?: return
-        Picasso.get().load(profile.imageUrl).into(profile_image_view as ImageView)
+        Picasso.get().load(profile.imageUrl).into(profile_image_view)
         first_name_edit_text.setText(profile.firstName)
         last_name_edit_text.setText(profile.lastName)
         email_edit_text.setText(profile.email)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        finish()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.edit_profile_options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_menu_item -> finish()
+            android.R.id.home -> {
+                AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.confirmation))
+                    .setMessage("Do you really want to exit?")
+                    .setPositiveButton(getString(R.string.ok)) { _, _ -> finish() }
+                    .setNegativeButton(getString(R.string.cancel)) { _, _ -> }
+                    .show()
+            }
+        }
         return true
     }
 }
