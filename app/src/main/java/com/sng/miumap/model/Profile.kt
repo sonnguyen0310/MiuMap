6d28f928735e0bdb473b9a4e76302c68693bc265
Kanharith SOK <kanharithsok@gmail.com>
2020-03-10 22:23:18 -0500

    Added imageView to frament_profile

--- app/src/main/java/com/sng/miumap/model/Profile.kt
+++ app/src/main/java/com/sng/miumap/model/Profile.kt
@@ -1,6 +1,6 @@
 package com.sng.miumap.model
 
-data class Profile(val id: String, val firstName: String, val lastName: String, val email: String) {
+data class Profile(val id: String, val firstName: String, val lastName: String, val email: String, val imageUrl: String) {
 
     fun fullName() = "$firstName $lastName"
 
--- app/src/main/java/com/sng/miumap/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/sng/miumap/ui/profile/ProfileFragment.kt
@@ -8,24 +8,41 @@ import android.widget.TextView
 import android.support.v4.app.Fragment
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.widget.ImageButton
+import android.widget.ImageView
 import com.sng.miumap.R
+import com.sng.miumap.model.Profile
+import com.squareup.picasso.Picasso
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var avatarImageView: ImageView
+    private lateinit var nameTextView: TextView
+    private lateinit var updatePhotoImageButton: ImageButton
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        initSubviews(root)
+
+        val profile = Profile(
+            "001", "Monkey", "D. Luffy", "hello@miu.edu",
+            "https://avatarfiles.alphacoders.com/125/thumb-125919.jpg"
+        )
+
+        Picasso.get().load(profile.imageUrl).into(avatarImageView)
+        nameTextView.text = profile.fullName()
+
         return root
     }
+
+    private fun initSubviews(view: View) {
+        avatarImageView = view.findViewById(R.id.avatar_image_view)
+        nameTextView = view.findViewById(R.id.name_text_view)
+        updatePhotoImageButton = view.findViewById(R.id.update_photo_image_button)
+    }
+
 }
